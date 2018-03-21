8b43deb1be9d9b65c1f756f95ff67aa88fad193e
Raih07 <kirill.r.2010.stud@gmail.com>
2018-03-21 19:56:38 +0300

    Версия со шрифтом ArialMT

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,16 @@
 var color_list = document.getElementById('color_list');
+//var image_catalog = document.getElementById('image_catalog');
 
-color_list.addEventListener('click', selectColor);
+if (color_list) {
+  color_list.addEventListener('click', selectColor);
+}
 
 function selectColor(event) {
   var color_item = event.target.closest('.catalog-item__color-item');
   if (color_item && color_list.contains(color_item)) {
     deselectAllColor(this);
     color_item.classList.add('catalog-item__color-item--active');
+    //showImage(color_item.dataset.src);
   }
 }
 
@@ -14,4 +18,8 @@ function deselectAllColor(items) {
   for (var i = 0; i < items.children.length; i++) {
     items.children[i].classList.remove('catalog-item__color-item--active');
   }
+}
+
+function showImage(href) {
+  image_catalog.src = href;
 }
